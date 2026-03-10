40bc579daaf592b67c997a1802d9b44bd2202c2d
PolenzaniPietro <polenzani.pietro@franchettisalviani.net>
2026-03-10 18:58:41 +0100

    modificate le classi giocatore e Manager
    -rimossa temporaneamente la funzione thread che dava alcuni problemi

--- gioco_talpa/src/gioco_talpa/Giocatore.java
+++ gioco_talpa/src/gioco_talpa/Giocatore.java
@@ -1,21 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package gioco_talpa;
 
-/**
- *
- * @author polenzani.pietro
- */
 public class Giocatore {
-    private String name;
+
     private int points;
-    public void colpisciTalpa(Talpa talpa){
-        
+
+    public Giocatore() {
+        points = 0;
     }
-    
-    public void addPoints(int p){
-        this.points+=p;
+
+    public void addPoints(int p) {
+        points += p;
     }
+
+    public int getPoints() {
+        return points;
+    }
+
 }
--- gioco_talpa/src/gioco_talpa/Manager.java
+++ gioco_talpa/src/gioco_talpa/Manager.java
@@ -1,71 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package gioco_talpa;
 
-import java.util.ArrayList;
 import java.util.Random;
-import java.util.Timer;
 
-/**
- *
- * @author polenzani.pietro
- */
 public class Manager {
-    private Timer gameTime;
-    private int score;
-    private Difficulty diff;
+
     private int bucaAttiva;
-    Random rnd;
-    
+    private Random rnd;
     private Talpa talpa;
     private Buca[] buche;
     private Giocatore player;
-    
-    
+
     public Manager() {
-    rnd = new Random();
-    bucaAttiva = -1;
-    buche = new Buca[9];
-    for (int i = 0; i < buche.length; i++) {
-         buche[i] = new Buca();
-    }    
-    talpa = new Talpa(false, 10, 1000);
-    player = new Giocatore();
-}
+        rnd = new Random();
+        bucaAttiva = -1;
+        buche = new Buca[9];
+        for (int i = 0; i < buche.length; i++) {
+            buche[i] = new Buca();
+        }
+        talpa = new Talpa(false, 10, 1000);
+        player = new Giocatore();
+    }
     
-    
-        public void spawnTalpa() {
+    public void spawnTalpa() {
+
         if (bucaAttiva != -1) {
             buche[bucaAttiva].setIsFree(false);
-            talpa.escape();
         }
+
         bucaAttiva = rnd.nextInt(9);
+
         buche[bucaAttiva].setIsFree(true);
         talpa.compariTalpa();
     }
-        
+    
     public void checkMole(int index) {
-        if (buche[index].getIsFree()) {
+        if (index == bucaAttiva) {
             talpa.colpita();
             player.addPoints(talpa.getValue());
             buche[index].setIsFree(false);
             bucaAttiva = -1;
         }
     }
-    
-     public void cicloTalpa() {
-        Thread t = new Thread(() -> {
-            while (true) {
-                try {
-                    spawnTalpa();
-                    Thread.sleep(rnd.nextInt(1000, 2001));
-                } catch (InterruptedException e) {
-                    break;
-                }
-            }
-        });
-        t.start();
+    public int getBucaAttiva() {
+        return bucaAttiva;
     }
+    public int getScore() {
+        return player.getPoints();
+    }
+
 }
